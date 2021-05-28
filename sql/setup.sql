5e05d864a1ffa7a23ce6325ddacaa06b60503011
Breeann Bolinsky <bolinskybm10@gmail.com>
2021-05-28 15:44:09 -0700

    heroku has successfully connected, routes are functional

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS short_urls;
 
 CREATE TABLE short_urls (
-  id TEXT PRIMARY KEY,
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   original_url TEXT NOT NULL
 );
+
+INSERT INTO short_urls (original_url) VALUES ('www.testurl.com')
