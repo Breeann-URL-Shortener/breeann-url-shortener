f5019af89c5bbd00632a1eae2670830d4207bd3d
Breeann Bolinsky <bolinskybm10@gmail.com>
2021-05-30 16:42:59 -0700

    added auth routes, user model, sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,16 @@
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS short_urls;
+
+CREATE TABLE users(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
 
 CREATE TABLE short_urls (
   id TEXT PRIMARY KEY,
-  original_url TEXT NOT NULL
+  original_url TEXT NOT NULL,
+  user_id BIGINT REFERENCES users(id) NOT NULL
 );
 
 -- INSERT INTO short_urls (original_url) VALUES ('www.testurl.com')
